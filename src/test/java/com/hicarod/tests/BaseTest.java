894a2e9e7c9aca58f46a9b3dc6356150d751bd59
HicaroD <hdanrlley1@gmail.com>
2023-07-15 14:53:17 -0300

    proj: refactoring code

---
+++ src/test/java/com/hicarod/tests/BaseTest.java
@@ -0,0 +1,25 @@
+package com.hicarod.tests;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public abstract class BaseTest {
+  protected static WebDriver driver;
+  private static String baseUrl = "https://www.google.com";
+  private static String driverPath = "src/test/java/com/hicarod/resources/geckodriver";
+
+  @BeforeClass
+  public static void setUp() {
+    System.setProperty("webdriver.firefox.driver", driverPath);
+    driver = new FirefoxDriver();
+    driver.manage().window().maximize();
+    driver.get(baseUrl);
+  }
+
+  @AfterClass
+  public static void tearDown() {
+    driver.quit();
+  }
+}
--- src/test/java/com/hicarod/tests/GoogleTest.java
+++ src/test/java/com/hicarod/tests/GoogleTest.java
@@ -1,29 +1,15 @@
 package com.hicarod.tests;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.List;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-import junit.framework.TestCase;
-
-public class GoogleTest extends TestCase {
-  private WebDriver driver;
-  private String baseUrl = "https://www.google.com";
-  private String driverPath = "src/test/java/com/hicarod/resources/geckodriver";
-
-  @Override
-  protected void setUp() throws Exception {
-    System.setProperty("webdriver.firefox.driver", driverPath);
-    driver = new FirefoxDriver();
-    driver.manage().window().maximize();
-    driver.get(baseUrl);
-  }
-
+public class GoogleTest extends BaseTest {
   @Test
   public void testMustSearchOnGoogle() throws Exception {
     // Obtem o campo de input
@@ -42,10 +28,5 @@ public class GoogleTest extends TestCase {
 
     // Garanta que existe um span com Corinthians dentro
     assertTrue(hasCorinthiansTextField);
-  }
-
-  @Override
-  protected void tearDown() throws Exception {
-    driver.quit();
   }
 }
